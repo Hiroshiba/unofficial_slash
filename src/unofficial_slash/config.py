Hiroshiba/yukarin_esosoav

feat(dataset): UPathを使用してデータパスの処理を改善し、ローカルパスのキャッシュ機能を追加

src/hiho_pytorch_base/config.py
@@ -3,22 +3,27 @@
 from pathlib import Path
 from typing import Any, Self
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 
 from hiho_pytorch_base.utility.git_utility import get_branch_name, get_commit_id
+from hiho_pytorch_base.utility.type_utility import UPathField
 
 
-class DataFileConfig(BaseModel):
+class _Model(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+class DataFileConfig(_Model):
     """データファイルの設定"""
 
-    f0_pathlist_path: Path
-    volume_pathlist_path: Path
-    lab_pathlist_path: Path
-    speaker_dict_path: Path
-    root_dir: Path | None
+    f0_pathlist_path: UPathField
+    volume_pathlist_path: UPathField
+    lab_pathlist_path: UPathField
+    speaker_dict_path: UPathField
+    root_dir: UPathField | None
 
 
-class DatasetConfig(BaseModel):
+class DatasetConfig(_Model):
     """データセット全体の設定"""
 
     train: DataFileConfig
@@ -29,7 +34,7 @@ class DatasetConfig(BaseModel):
     seed: int = 0
 
 
-class NetworkConfig(BaseModel):
+class NetworkConfig(_Model):
     """ニューラルネットワークの設定"""
 
     phoneme_size: int
@@ -43,13 +48,13 @@ class NetworkConfig(BaseModel):
     input_phoneme_duration: bool
 
 
-class ModelConfig(BaseModel):
+class ModelConfig(_Model):
     """モデルの設定"""
 
     pass
 
 
-class TrainConfig(BaseModel):
+class TrainConfig(_Model):
     """学習の設定"""
 
     batch_size: int
@@ -68,7 +73,7 @@ class TrainConfig(BaseModel):
     use_amp: bool = True
 
 
-class ProjectConfig(BaseModel):
+class ProjectConfig(_Model):
     """プロジェクトの設定"""
 
     name: str
@@ -76,7 +81,7 @@ class ProjectConfig(BaseModel):
     category: str | None = None
 
 
-class Config(BaseModel):
+class Config(_Model):
     """機械学習の全設定"""
 
     dataset: DatasetConfig

src/hiho_pytorch_base/dataset.py
@@ -7,8 +7,11 @@ from enum import Enum
 from pathlib import Path
 from typing import assert_never
 
+import fsspec
+from fsspec.implementations.local import LocalFileSystem
 from pydantic import TypeAdapter
 from torch.utils.data import Dataset as BaseDataset
+from upath import UPath
 
 from hiho_pytorch_base.config import DataFileConfig, DatasetConfig
 from hiho_pytorch_base.data.data import (
@@ -20,21 +23,33 @@ from hiho_pytorch_base.data.phoneme import ArpaPhoneme
 from hiho_pytorch_base.data.sampling_data import SamplingData
 
 
+def _to_local_path(p: UPath) -> Path:
+    """リモートならキャッシュを作ってそのパスを、ローカルならそのままそのパスを返す"""
+    if isinstance(p.fs, LocalFileSystem):
+        return Path(p)
+    obj = fsspec.open_local(
+        "simplecache::" + str(p), simplecache={"cache_storage": "./hiho_cache/"}
+    )
+    if isinstance(obj, list):
+        raise ValueError(f"複数のローカルパスが返されました: {p} -> {obj}")
+    return Path(obj)
+
+
 @dataclass
 class LazyInputData:
     """遅延読み込み対応の入力データ構造"""
 
-    f0_path: Path
-    volume_path: Path
-    lab_path: Path
+    f0_path: UPath
+    volume_path: UPath
+    lab_path: UPath
     speaker_id: int
 
     def generate(self) -> InputData:
         """ファイルからデータを読み込んでDatasetInputを生成"""
         return InputData(
-            phonemes=ArpaPhoneme.load_julius_list(self.lab_path),
-            f0_data=SamplingData.load(self.f0_path),
-            volume_data=SamplingData.load(self.volume_path),
+            phonemes=ArpaPhoneme.load_julius_list(_to_local_path(self.lab_path)),
+            f0_data=SamplingData.load(_to_local_path(self.f0_path)),
+            volume_data=SamplingData.load(_to_local_path(self.volume_path)),
             speaker_id=self.speaker_id,
         )
 
@@ -113,25 +128,25 @@ class DatasetCollection:
                 assert_never(type)
 
 
-PathMap = dict[str, Path]
+PathMap = dict[str, UPath]
 """パスマップ。stemをキー、パスを値とする辞書型"""
 
 
-def _load_pathlist(pathlist_path: Path, root_dir: Path) -> PathMap:
+def _load_pathlist(pathlist_path: UPath, root_dir: UPath) -> PathMap:
     """pathlistファイルを読み込みんでパスマップを返す。"""
     path_list = [root_dir / p for p in pathlist_path.read_text().splitlines()]
     return {p.stem: p for p in path_list}
 
 
 def get_data_paths(
-    root_dir: Path | None, pathlist_paths: list[Path]
+    root_dir: UPath | None, pathlist_paths: list[UPath]
 ) -> tuple[list[str], list[PathMap]]:
     """複数のpathlistファイルからstemリストとパスマップを返す。整合性も確認する。"""
     if len(pathlist_paths) == 0:
         raise ValueError("少なくとも1つのpathlist設定が必要です")
 
     if root_dir is None:
-        root_dir = Path(".")
+        root_dir = UPath(".")
 
     path_mappings: list[PathMap] = []
 

Hiroshiba/unofficial_slash

src/unofficial_slash/config.py
@@ -3,22 +3,27 @@
 from pathlib import Path
 from typing import Any, Self
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 
 from unofficial_slash.utility.git_utility import get_branch_name, get_commit_id
+from unofficial_slash.utility.type_utility import UPathField
 
 
-class DataFileConfig(BaseModel):
+class _Model(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+class DataFileConfig(_Model):
     """SLASH 音声データファイルの設定"""
 
-    audio_pathlist_path: Path
-    pitch_label_pathlist_path: Path | None
-    length_file_path: Path
+    audio_pathlist_path: UPathField
+    pitch_label_pathlist_path: UPathField | None
+    length_file_path: UPathField
     batch_bins: int
-    root_dir: Path | None
+    root_dir: UPathField | None
 
 
-class DatasetConfig(BaseModel):
+class DatasetConfig(_Model):
     """SLASH データセット全体の設定"""
 
     train: DataFileConfig
@@ -32,7 +37,7 @@ class DatasetConfig(BaseModel):
     pitch_shift_range: int
 
 
-class NetworkConfig(BaseModel):
+class NetworkConfig(_Model):
     """SLASH Pitch Encoder ネットワークの設定"""
 
     # 基本設定
@@ -73,7 +78,7 @@ class NetworkConfig(BaseModel):
     vuv_threshold: float
 
 
-class ModelConfig(BaseModel):
+class ModelConfig(_Model):
     """SLASH 損失関数の設定"""
 
     # 基本損失重み
@@ -108,7 +113,7 @@ class ModelConfig(BaseModel):
     volume_change_db_range: float
 
 
-class TrainConfig(BaseModel):
+class TrainConfig(_Model):
     """学習の設定"""
 
     batch_size: int
@@ -130,7 +135,7 @@ class TrainConfig(BaseModel):
     use_amp: bool = True
 
 
-class ProjectConfig(BaseModel):
+class ProjectConfig(_Model):
     """プロジェクトの設定"""
 
     name: str
@@ -138,7 +143,7 @@ class ProjectConfig(BaseModel):
     category: str | None = None
 
 
-class Config(BaseModel):
+class Config(_Model):
     """機械学習の全設定"""
 
     dataset: DatasetConfig

src/unofficial_slash/dataset.py
@@ -7,26 +7,41 @@ from enum import Enum
 from pathlib import Path
 from typing import assert_never
 
+import fsspec
 import numpy
 import torchaudio
+from fsspec.implementations.local import LocalFileSystem
 from torch.utils.data import Dataset as BaseDataset
+from upath import UPath
 
 from unofficial_slash.config import DataFileConfig, DatasetConfig
 from unofficial_slash.data.data import InputData, OutputData, preprocess
 from unofficial_slash.sampler import load_lengths_from_file
 
 
+def _to_local_path(p: UPath) -> Path:
+    """リモートならキャッシュを作ってそのパスを、ローカルならそのままそのパスを返す"""
+    if isinstance(p.fs, LocalFileSystem):
+        return Path(p)
+    obj = fsspec.open_local(
+        "simplecache::" + str(p), simplecache={"cache_storage": "./hiho_cache/"}
+    )
+    if isinstance(obj, list):
+        raise ValueError(f"複数のローカルパスが返されました: {p} -> {obj}")
+    return Path(obj)
+
+
 @dataclass
 class LazyInputData:
     """遅延読み込み対応の入力データ構造"""
 
-    audio_path: Path
-    pitch_label_path: Path | None
+    audio_path: UPath
+    pitch_label_path: UPath | None
 
     def generate(self, *, sample_rate: int) -> InputData:
         """ファイルからデータを読み込んでInputDataを生成"""
         # 音声ファイル読み込み
-        audio, sr = torchaudio.load(self.audio_path)
+        audio, sr = torchaudio.load(_to_local_path(self.audio_path))
         assert sr == sample_rate, f"Expected sample rate {sample_rate}, but got {sr}"
 
         # モノラル変換（ステレオの場合は左チャンネルのみ使用）
@@ -36,13 +51,13 @@ class LazyInputData:
         # ピッチラベル読み込み（オプション）
         pitch_data = None
         if self.pitch_label_path is not None:
-            pitch_data = numpy.loadtxt(self.pitch_label_path)
+            pitch_data = numpy.loadtxt(_to_local_path(self.pitch_label_path))
 
         return InputData(
             audio=audio.squeeze(0).numpy(),  # (T,) 音声波形
-            pitch_label=pitch_data.astype(numpy.float32)
-            if pitch_data is not None
-            else None,  # (T,) ピッチラベル
+            pitch_label=(
+                pitch_data.astype(numpy.float32) if pitch_data is not None else None
+            ),  # (T,) ピッチラベル
         )
 
 
@@ -122,25 +137,25 @@ class DatasetCollection:
                 assert_never(type)
 
 
-PathMap = dict[str, Path]
+PathMap = dict[str, UPath]
 """パスマップ。stemをキー、パスを値とする辞書型"""
 
 
-def _load_pathlist(pathlist_path: Path, root_dir: Path) -> PathMap:
+def _load_pathlist(pathlist_path: UPath, root_dir: UPath) -> PathMap:
     """pathlistファイルを読み込みんでパスマップを返す。"""
     path_list = [root_dir / p for p in pathlist_path.read_text().splitlines()]
     return {p.stem: p for p in path_list}
 
 
 def get_data_paths(
-    root_dir: Path | None, pathlist_paths: list[Path]
+    root_dir: UPath | None, pathlist_paths: list[UPath]
 ) -> tuple[list[str], list[PathMap]]:
     """複数のpathlistファイルからstemリストとパスマップを返す。整合性も確認する。"""
     if len(pathlist_paths) == 0:
         raise ValueError("少なくとも1つのpathlist設定が必要です")
 
     if root_dir is None:
-        root_dir = Path(".")
+        root_dir = UPath(".")
 
     path_mappings: list[PathMap] = []
 